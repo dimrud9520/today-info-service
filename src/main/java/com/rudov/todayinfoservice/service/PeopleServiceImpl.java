d0f5a9d70cce1534bbdaacef1ab4aeeff30a0ae4
d.rudov <dmitrijrudov4@gmail.com>
2023-06-01 19:09:43 +0300

    empty

--- src/main/java/com/rudov/todayinfoservice/service/PeopleServiceImpl.java
+++ src/main/java/com/rudov/todayinfoservice/service/PeopleServiceImpl.java
@@ -20,6 +20,7 @@ public class PeopleServiceImpl implements PeopleService {
         var people = getPeople(id);
         people.setName(newName);
         updatePeople(people);
+        //todo надо откатить
     }
 
     @Override
